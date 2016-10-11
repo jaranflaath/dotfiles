36016c409f42fb115e2c2aca2ee36e71eb9a2d33
Jaran Flaath <jaran.nilsen@gmail.com>
2016-10-11 12:39:42 +0200

    Update software.sh

--- ubuntu/software.sh
+++ ubuntu/software.sh
@@ -5,3 +5,5 @@ sudo apt-get install meld -y
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
 sudo apt-get install git -y
+sudo apt-get install fish -y
+curl -L http://get.oh-my.fish | fish
