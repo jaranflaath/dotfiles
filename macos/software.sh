e30e2ce7046f222d5377d4f30da55f3343499c24
Jaran Flaath <jaran.flaath@gmail.com>
2016-11-22 21:10:20 +0100

    Updates to macOS scripts

--- macos/software.sh
+++ macos/software.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 echo "Installing software..."
+
+# Upgarde brew, install python and pip
+brew update
 brew install python
 pip install --user git+https://github.com/Lokaltog/powerline
 cp ~/.profile ~/.profile_backup
+
+#Oh my fish
 curl -L http://get.oh-my.fish | fish
-fish -c "omf install bobthefish"
+fish -c "omf install bobthefish && exit"
 echo "" >> ~/.config/fish/config.fish
 echo 'set -g theme_date_format "+%H:%M:%S"' >> ~/.config/fish/config.fish
