abfe303ee28a66d2ab12f8ed0288663bacdfc860
Jaran Flaath <jaran.flaath@gmail.com>
2017-01-19 22:57:15 +0100

    Fix execution

--- common/git-config.sh
+++ common/git-config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>;%Creset' --abbrev-commit --date=relative"
 git config --global alias.st status
 git config --global alias.f fetch
