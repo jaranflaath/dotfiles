40e2a20d372af68b59cec61a5962f0bb96ba3871
Jaran Flaath <jaran@Jarans-MacBook-Pro.local>
2017-01-19 22:55:08 +0100

    Update software installation on mac

--- macos/software.sh
+++ macos/software.sh
@@ -3,13 +3,17 @@
 echo "Installing software..."
 
 # Upgarde brew, install python and pip
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew install python
 pip install --user git+https://github.com/Lokaltog/powerline
 cp ~/.profile ~/.profile_backup
 
 #Oh my fish
-curl -L http://get.oh-my.fish | fish
-fish -c "omf install bobthefish && exit"
+brew install fish
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -XGET https://github.com/powerline/fonts/blob/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf?raw=true > ~/Downloads/Meslo_Powerline.otf
+echo "Please install the downloaded Powerline font"
+fish -c "omf install bobthefish; exit"
 echo "" >> ~/.config/fish/config.fish
 echo 'set -g theme_date_format "+%H:%M:%S"' >> ~/.config/fish/config.fish
