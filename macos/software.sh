50aa3eab8822010bd830c8ac339a4d37d2533a5e
Jaran Flaath <jaran.flaath@gmail.com>
2017-09-29 15:19:31 +0200

    Add installation of omf

--- macos/software.sh
+++ macos/software.sh
@@ -12,6 +12,7 @@ cp ~/.profile ~/.profile_backup
 #Oh my fish
 brew install fish
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -L https://get.oh-my.fish | fish
 curl -XGET https://github.com/powerline/fonts/blob/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf?raw=true > ~/Downloads/Meslo_Powerline.otf
 echo "Please install the downloaded Powerline font"
 fish -c "omf install bobthefish; exit"
