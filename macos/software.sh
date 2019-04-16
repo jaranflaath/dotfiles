355caf2d76145f611312a4587c78f4e6f4b35b33
Jaran Flaath <jaran.flaath@agens.no>
2019-04-16 08:53:10 +0200

    Add installation of git

--- macos/software.sh
+++ macos/software.sh
@@ -5,6 +5,7 @@ echo "Installing software..."
 # Upgarde brew, install python and pip
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+brew install git
 brew install python
 pip install --user git+https://github.com/Lokaltog/powerline
 cp ~/.profile ~/.profile_backup
@@ -12,7 +13,7 @@ cp ~/.profile ~/.profile_backup
 #Oh my fish
 brew install fish
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl -L https://get.oh-my.fish | fish
+curl -L https://get.oh-my.fish
 curl -XGET https://github.com/powerline/fonts/blob/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf?raw=true > ~/Downloads/Meslo_Powerline.otf
 echo "Please install the downloaded Powerline font"
 fish -c "omf install bobthefish; exit"
